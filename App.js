332b511357d4f1514e20653dbe991b1097162c19
jyotideshmukh <jyotipawar2003@gmail.com>
2024-01-27 18:22:50 +0530

    Created siblings html tags using react, we need to give array

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 /*
-const heading = React.createElement('h1',{id:"heading", otherattribute:"abc"},'Hello world from React');
+const heading = React.createElement("h1",{id:"heading", otherattribute:"abc"},"Hello world from React");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 */
 
 // now create nested tags is react
 
-const parent = React.createElement('div',
-                      {id: "paranet"}, 
-                    React.createElement('div', {id:'child'},
-                    React.createElement('h1',{id:'heading'},'I am h1 tag'
-                    )));
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(parent);
+/*
+const parent = React.createElement("div",
+    { id: "paranet" },
+    React.createElement("div", { id: "child" },
+        React.createElement("h1", { id: "heading" }, "I am h1 tag"
+        )));
+        */
+
+        /*  How to add siblings */
+        const parent = React.createElement("div",
+        { id: "paranet" },
+        React.createElement("div", { id: "child" },
+            [
+                React.createElement("h1", { id: "heading" }, "I am h1 tag"),
+                React.createElement("h2", { id: "heading2" }, "I am h2 tag")
+            ]
+            ));       
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
