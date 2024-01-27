2024a06cd8c8cfa36042d6c464cb8be831ec66ca
jyotideshmukh <jyotipawar2003@gmail.com>
2024-01-27 18:17:21 +0530

    Added nested html tags using react and created css

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
+/*
 const heading = React.createElement('h1',{id:"heading", otherattribute:"abc"},'Hello world from React');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
+*/
+
+// now create nested tags is react
+
+const parent = React.createElement('div',
+                      {id: "paranet"}, 
+                    React.createElement('div', {id:'child'},
+                    React.createElement('h1',{id:'heading'},'I am h1 tag'
+                    )));
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(parent);
